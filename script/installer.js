ff8d6a19f9282da9ad359d21909b9442bdda518c
jgarza9788 <jgarza9788@gmail.com>
2017-10-12 00:35:57 -0700

    Most things Compile Now!

--- script/installer.js
+++ script/installer.js
@@ -2,6 +2,37 @@
 
 const createWindowsInstaller = require('electron-winstaller').createWindowsInstaller
 const path = require('path')
+
+getInstallerConfig()
+     .then(createWindowsInstaller)
+     .catch((error) => {
+     console.error(error.message || error)
+     process.exit(1)
+ })
+
+function getInstallerConfig () {
+    console.log('creating windows installer')
+    // const rootPath = path.join('./')
+    // const outPath = path.join(rootPath, 'release-builds')
+    const rootPath = path.join(__dirname, '..')
+    const outPath = path.join(rootPath, 'out')
+
+    // console.log(path.join(rootPath, 'assets', 'app-icons', 'win', 'app.ico'))
+
+    return Promise.resolve({
+      appDirectory: path.join(outPath, 'quark-win32-ia32'),
+      authors: 'Justin Garza',
+      noMsi: true,
+      outputDirectory: path.join(outPath, 'windows-installer'),
+      exe: 'Quark.exe',
+      setupExe: 'Quark_Installer.exe',
+      setupIcon: path.join(rootPath, 'assets', 'app-icon', 'win', 'app.ico')
+   })
+}
+
+/*
+const createWindowsInstaller = require('electron-winstaller').createWindowsInstaller
+const path = require('path')
 const rimraf = require('rimraf')
 
 const appName = 'Quark'
@@ -20,7 +51,7 @@ function getInstallerConfig () {
 
   return Promise.resolve({
     appDirectory: path.join(outPath, appName + '-win32-ia32'),
-    exe: appName + '.exe',
+    exe: appName +'.exe',
     iconUrl: 'https://raw.githubusercontent.com/electron/electron-api-demos/master/assets/app-icon/win/app.ico',
     loadingGif: path.join(rootPath, 'assets', 'img', 'loading.gif'),
     noMsi: true,
@@ -38,3 +69,4 @@ function deleteOutputFolder () {
     })
   })
 }
+*/
